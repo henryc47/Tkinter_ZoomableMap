4c49a248da410a7548d684e0127d618f388894d9
Henry Chadban <henrychadban@Henrys-Mac-mini.modem>
2023-04-17 18:20:54 +1000

    added in code to run an example which will be useful for debugging, at present example only has code to select the type of example

--- use_example.py
+++ use_example.py
@@ -1,1 +1,26 @@
 #an example of how the zoom_map library should be used
+
+import pandas as pd #for loading node and line info  from csv files
+
+
+
+
+def example():
+    while True: #wait till valid mode selected
+        select_mode = input("Are we using 'manual' or node 'lines': ")
+        if select_mode == 'manual':
+            print("manual lines selected")
+            break
+        elif select_mode == 'node':
+            print('node lines selected')
+            break
+        else:
+            print(select_mode," not a valid mode, please type 'manual' or 'node' ")
+
+    print('foo')
+
+
+
+
+if __name__=="__main__":
+    example()
